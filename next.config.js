9269d86457b3e57292ce483d74a66b134ccf2bd6
pavanbhaskar <pavanbhaskardev@gmail.com>
2024-03-19 18:43:56 +0530

    feat(nextjs config change)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
